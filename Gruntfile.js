be8efdd8f91c57f7003d897159e9ac7e045a5bbe
Hack Reactor Students <accounts@hackreactor.com>
2016-02-10 19:59:34 -0800

    take out separator

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       options: {
-        separator: ';',
+        separator: '',
       },
       dist: {
         src: ['public/client/**/*.js'],
