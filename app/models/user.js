d88b4082eacf577b2890badcd139877b9a868ce1
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 19:49:44 -0800

    refactor work, not working

--- app/models/user.js
+++ app/models/user.js
@@ -2,25 +2,6 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-// var User = db.Model.extend({
-  // tableName: 'users',
-  // hasTimestamps: true,
-  // initialize: function() {
-  //   this.on('creating', this.hashPassword);
-  // },
-  // comparePassword: function(attemptedPassword, callback) {
-  //   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-  //     callback(isMatch);
-  //   });
-  // },
-  // hashPassword: function() {
-  //   var cipher = Promise.promisify(bcrypt.hash);
-  //   return cipher(this.get('password'), null, null).bind(this)
-  //     .then(function(hash) {
-  //       this.set('password', hash);
-  //     });
-  // }
-// });
 
 var usernameSchema = new db.Schema({
   username: {
@@ -37,28 +18,18 @@ usernameSchema.pre('save', function() {
   var cipher = Promise.promisify(bcrypt.hash);
     return cipher(this.password, null, null).bind(this)
       .then(function(hash) {
-        this.set('password', hash);
+        this.password = hash;
       });
   }
 );
 
 
 usernameSchema.method('comparePassword', function(attemptedPassword, callback) {
-      bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+      bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
         callback(isMatch);
       });
     }
 );
-//alternate form
-// usernameSchema.on('init', function () {
-//   // do stuff with the model
-//   var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.password, null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       }
-//     );
-// });
 
 
 
